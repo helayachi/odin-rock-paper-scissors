94ac46e41ca44a643da8ed2c5c9b027467ec165e
xelayh <hamritelayachi@gmail.com>
2025-11-11 21:35:18 +0100

    Add logic to get human/computer choice

---
+++ script.js
@@ -0,0 +1,7 @@
+// logic to get the computer choice
+const getComputerChoice = () => ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
+// logic to get the human choice
+const getPlayerChoice = () => +prompt("1) Rock 2) Paper 3) Scissors");
+// Check if the human input is valid
+const isValid = (input) => input !== NaN && input !== 0 && input >= 1 && input <= 3;
+
