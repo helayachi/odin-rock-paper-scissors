6bc703cc2eeb5cef06fda78368a7bb030914ad10
xelayh <hamritelayachi@gmail.com>
2025-11-11 22:26:57 +0100

    Refactor user input & validation

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-// logic to get the computer choice
-const getComputerChoice = () => ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
-// logic to get the human choice
-const getPlayerChoice = () => +prompt("1) Rock 2) Paper 3) Scissors");
-// Check if the human input is valid
-const isValid = (input) => input !== NaN && input !== 0 && input >= 1 && input <= 3;
+// Game options 
+const options = ['rock', 'paper', 'scissors'];
 
+// logic to get the computer choice
+const getComputerChoice = () => options[Math.floor(Math.random() * 3)];
+
+// Check if the human input is valid & in range
+const isValid = (input) => input !== "" && input !== " ";
+const inRange = (input) => input === "rock" || input === "paper" || input === "scissors";
+
+// logic to get the human choice
+function getPlayerChoice()  {
+    const input = prompt("Type: Rock Paper Scissors").toLowerCase();
+    return isValid(input) && inRange(input) ? input : getPlayerChoice();
+} 
+
+
+// human/computer score variables
 let humanScore = 0;
 let computerScore = 0;
+
+const computerChoice = getComputerChoice();
+const playerChoice = getPlayerChoice();
+
+
+function playRound()
