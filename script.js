d192a4ec828e3397addfc9043a662fa5c5ddaba4
xelayh <hamritelayachi@gmail.com>
2025-11-11 01:38:15 +0100

    Add initial pseudocode for game structure and round logic

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+/*
+Start the game
+
+Set roundCount = 0;
+Set playerScore = 0;
+Set computerScore = 0;
+
+While roundCount < 3
+    Step 1: Get computer choice
+    Step 2: Get player choice
+    Step 3: Compare choices
+    Step 4: Decide round winner
+    Step 5: Add 1 point to winner
+End while
+
+Compare playerScore and computerScore
+Decide game winner
+Alert the winner
+
+End game
+*/
